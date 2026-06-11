314b646cca22906dc98f44aa6c960a78226e2e91
kashish25052004 <kashishjaiswal25052004@gmail.com>
2026-06-11 14:17:53 +0530

    update followController

--- controllers/followController.js
+++ controllers/followController.js
@@ -1,11 +1,13 @@
 const asyncHandler = require('../utils/asyncHandler');
 const followService = require('../services/followService');
 
+//201 Created -> New resource created
 const followUser = asyncHandler(async (req, res) => {
   const follow = await followService.followUser(req.user._id, req.validated.params.userId);
   res.status(201).json({ follow });
 });
 
+//204 No Content -> Successfully processed the request, but no content to return
 const unfollowUser = asyncHandler(async (req, res) => {
   await followService.unfollowUser(req.user._id, req.validated.params.userId);
   res.status(204).send();
